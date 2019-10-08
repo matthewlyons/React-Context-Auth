01087d29f15817dd05b6fefc0f71ac7a5b3fb1f9
Matt Lyons <matthewwaynelyons@live.com>
2019-10-07 23:35:20 -0700

    readme

--- src/context/actions.js
+++ src/context/actions.js
@@ -2,7 +2,6 @@ import { types } from './reducers';
 
 export const useActions = (state, dispatch) => {
   function authSubmit(email, password) {
-    console.log(email, password);
     // post email and password to backend and get token
     let token = 'ExampleToken';
     dispatch({ type: types.AUTH_LOGIN, payload: token });
